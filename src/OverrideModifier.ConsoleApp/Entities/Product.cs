65456a11f050242f2c90e6eca46a81ddb2c0f22a
kauatwn <kaualopescontato@gmail.com>
2024-03-13 10:52:20 -0300

    Override modifier

---
+++ src/OverrideModifier.ConsoleApp/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace OverrideModifier.ConsoleApp.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public double TotalValueInStock()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, $ {Price.ToString("F2", CultureInfo.InvariantCulture)}, " +
+                $"{Quantity} unidades, Total: $" +
+                $"{TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ src/OverrideModifier.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using OverrideModifier.ConsoleApp.Entities;
+
+var p = new Product();
+
+Console.WriteLine("Escreva os dados do produto:");
+Console.Write("Nome: ");
+p.Name = Console.ReadLine();
+Console.Write("Preço: ");
+p.Price = double.Parse(Console.ReadLine());
+Console.Write("Quantidade em estoque: ");
+p.Quantity = int.Parse(Console.ReadLine());
+
+Console.WriteLine(p);
