8da3dcf23e23299c4af0211c598b1906166da1d0
kauatwn <kaualopescontato@gmail.com>
2024-03-13 15:52:16 -0300

    Parameter modifiers

---
+++ src/ParameterModifiers.ConsoleApp/Entities/Calculator.cs
@@ -0,0 +1,28 @@
+﻿namespace ParameterModifiers.ConsoleApp.Entities
+{
+    internal static class Calculator
+    {
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        // Não se utiliza return com ref, pois a variável será referenciada
+        public static void Triple(ref int number)
+        {
+            number *= 3;
+        }
+
+        // Não se utiliza return com out, pois o resultado e o resto da divisão será referenciado
+        public static void Divide(double dividend, double divisor, out double result, out double remainder)
+        {
+            result = dividend / divisor;
+            remainder = dividend % divisor;
+        }
+    }
+}
---
+++ src/ParameterModifiers.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using ParameterModifiers.ConsoleApp.Entities;
+
+int sumResult = Calculator.Sum(5, 5);
+Console.WriteLine(sumResult);
+
+int value = 5;
+
+// Com o ref, o valor é atribuído à mesma variável como referência
+Calculator.Triple(ref value);
+Console.WriteLine($"O valor triplicado é: {value}");
+
+double dividend = 10;
+double divisor = 2;
+double divisionResult;
+double remainder;
+
+Calculator.Divide(dividend, divisor, out divisionResult, out remainder);
+Console.WriteLine($"{dividend} dividido por {divisor} é igual a {divisionResult}");
+Console.WriteLine($"Resto da divisão: {remainder}");
