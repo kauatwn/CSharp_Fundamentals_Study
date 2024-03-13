4a6528b1ba6bfc52779b1d5a0d84a381a38aba7f
kauatwn <kaualopescontato@gmail.com>
2024-03-13 10:55:18 -0300

    Auto properties

---
+++ src/AutoProperties.ConsoleApp/Entities/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoProperties.ConsoleApp.Entities
+{
+    internal class Person(string firstName, string lastName, int age)
+    {
+        // Auto properties são mais concisas e reduzem a quantidade de código boilerplate
+        public string FirstName { get; set; } = firstName;
+        public string LastName { get; set; } = lastName;
+        public int Age { get; set; } = age;
+    }
+}
---
+++ src/AutoProperties.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using AutoProperties.ConsoleApp.Entities;
+
+Console.WriteLine("Digite seu primeiro nome:");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Digite seu último nome:");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Digite sua idade:");
+int age = int.Parse(Console.ReadLine());
+
+var p = new Person(firstName, lastName, age);
+
+Console.WriteLine($"Olá, {p.FirstName} {p.LastName}! Você tem {p.Age} anos.");
