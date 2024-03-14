f751f13aa89f5e661a54c1033fc8447a331da5d5
kauatwn <kaualopescontato@gmail.com>
2024-03-14 10:28:29 -0300

    Virtual modifier

---
+++ src/VirtualModifier.ConsoleApp/Entities/Account.cs
@@ -0,0 +1,21 @@
+﻿namespace VirtualModifier.ConsoleApp.Entities
+{
+    internal class Account(int number, string holder, double balance)
+    {
+        public int Number { get; private set; } = number;
+        public string Holder { get; private set; } = holder;
+        public double Balance { get; protected set; } = balance;
+
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
+
+// O modificador virtual permite que método da classe base (Account) seja modificada em subclasses (SavingsAccount).
