2603daecfd1a391c626d71ae3abf1673c430150e
kauatwn <kaualopescontato@gmail.com>
2024-03-14 10:32:10 -0300

    Sealed modifier

---
+++ src/SealedModifier.ConsoleApp/Entities/SavingsAccount.cs
@@ -0,0 +1,19 @@
+﻿namespace SealedModifier.ConsoleApp.Entities
+{
+    internal sealed class SavingsAccount(int number, string holder, double balance, double interestRate) : Account(number, holder, balance)
+    {
+        public double InterestRate { get; set; } = interestRate;
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public sealed override void Withdraw(double amount)
+        {
+            // Chama o método da superclasse (Account) e desconta mais 2.
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
---
+++ src/SealedModifier.ConsoleApp/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,12 @@
+﻿namespace SealedModifier.ConsoleApp.Entities
+{
+    // Não é possível herdar por causa do modificador sealed.
+    internal class SavingsAccountPlus //: SavingsAccount
+    {
+        // Não é possível sobrepor o método por causa do modificador sealed.
+        //public override Withdraw(double amount)
+        //{
+
+        //}
+    }
+}
