36841357b02f4cad40e85f00da5569e20b055637
kauatwn <kaualopescontato@gmail.com>
2024-03-28 12:33:40 -0300

    Parameter modifiers

--- src/ParameterModifiers.ConsoleApp/Entities/Calculator.cs
+++ src/ParameterModifiers.ConsoleApp/Entities/Calculator.cs
@@ -12,13 +12,13 @@
             return sum;
         }
 
-        // Não se utiliza return com ref, pois a variável será referenciada
+        // Não se utiliza return com ref, pois a variável será referenciada.
         public static void Triple(ref int number)
         {
             number *= 3;
         }
 
-        // Não se utiliza return com out, pois o resultado e o resto da divisão será referenciado
+        // Não se utiliza return com out, pois o resultado e o resto da divisão será referenciado.
         public static void Divide(double dividend, double divisor, out double result, out double remainder)
         {
             result = dividend / divisor;
--- src/ParameterModifiers.ConsoleApp/Program.cs
+++ src/ParameterModifiers.ConsoleApp/Program.cs
@@ -5,15 +5,18 @@ Console.WriteLine(sumResult);
 
 int value = 5;
 
-// Com o ref, o valor é atribuído à mesma variável como referência
+// Com o ref, o valor é atribuído à mesma variável como referência.
+// Necessário inicializar a variável quando se utiliza ref.
 Calculator.Triple(ref value);
 Console.WriteLine($"O valor triplicado é: {value}");
 
 double dividend = 10;
 double divisor = 2;
-double divisionResult;
-double remainder;
 
-Calculator.Divide(dividend, divisor, out divisionResult, out remainder);
+// Não é necessário inicializar as variáveis quando se utiliza out.
+//double divisionResult;
+//double remainder;
+
+Calculator.Divide(dividend, divisor, out double divisionResult, out double remainder);
 Console.WriteLine($"{dividend} dividido por {divisor} é igual a {divisionResult}");
 Console.WriteLine($"Resto da divisão: {remainder}");
