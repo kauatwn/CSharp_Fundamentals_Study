3a18d8968b9ee59d363cfd7601a8a0321e6ed472
kauatwn <kaualopescontato@gmail.com>
2024-04-14 18:37:02 -0300

    GetHashCode and Equals

---
+++ src/GetHashCodeAndEquals.ConsoleApp/Entities/Animal.cs
@@ -0,0 +1,21 @@
+﻿namespace GetHashCodeAndEquals.ConsoleApp.Entities
+{
+    internal class Animal
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Breed { get; set; } = string.Empty;
+
+        // Verifica se as raças são iguais
+        public override bool Equals(object? obj)
+        {
+            return obj is Animal animal &&
+                   Breed == animal.Breed;
+        }
+
+        public override int GetHashCode()
+        {
+            return Breed.GetHashCode();
+        }
+    }
+}
