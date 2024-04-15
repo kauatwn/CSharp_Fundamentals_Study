a9a38f95bc690757c6f43ed24992ee844a855dc8
kauatwn <kaualopescontato@gmail.com>
2024-04-15 19:03:59 -0300

    HashSet

---
+++ src/HashSet.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿var set = new HashSet<string>
+{
+    "TV",
+    "Notebook",
+    "Tablet"
+};
+
+Console.WriteLine(set.Contains("Computer"));
+
+foreach (var item in set)
+{
+    Console.WriteLine(item);
+}
+
+// HashSet<T> é uma coleção não ordenada de itens distintos usadas para armazenar e manipular conjunto de dados não duplicados
