de34dcd27db0a989943b43cb2d16994a3bd5bdf5
kauatwn <kaualopescontato@gmail.com>
2024-04-15 22:06:35 -0300

    SortedDictionary

---
+++ src/SortedDictionary.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿var sortedDictionary = new SortedDictionary<int, string>
+{
+    { 3, "Manga" },
+    { 1, "Banana" },
+    { 2, "Laranja" }
+};
+
+foreach (var item in sortedDictionary)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+// SortedDictionary<TKey, TValue> é uma coleção genérica que armazena pares chave-valor ordenados com base na chave.
