9d3ee787aafef4f4542e9d378b6018ac06147baf
kauatwn <kaualopescontato@gmail.com>
2024-04-15 22:06:24 -0300

    Dictionary

---
+++ src/Dictionary.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿var dictionary = new Dictionary<int, string>
+{
+    {1,"Banana"},
+    {3, "Manga"},
+    {2, "Laranja"},
+    {4, "Abacate"},
+    {5,"Maça"}
+};
+
+foreach (var item in dictionary)
+{
+    Console.WriteLine("Chave: {0}, valor: {1}", item.Key, item.Value);
+}
+Console.WriteLine();
+
+// Dictionary<TKey, TValue> é uma coleção genérica que armazena pares chave-valor sem ordem específica.
