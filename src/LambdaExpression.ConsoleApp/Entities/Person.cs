0856cf07f776eca3addaa08b50c9f603c50f5c0d
kauatwn <kaualopescontato@gmail.com>
2024-04-16 21:17:12 -0300

    Lambda expression

---
+++ src/LambdaExpression.ConsoleApp/Entities/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace LambdaExpression.ConsoleApp.Entities
+{
+    internal class Person
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+}
---
+++ src/LambdaExpression.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using LambdaExpression.ConsoleApp.Entities;
+
+var people = new List<Person>
+{
+    new() { Name = "Alice", Age = 30 },
+    new() { Name = "Bob", Age = 20 },
+    new() { Name = "Charlie", Age = 40 },
+    new() { Name = "David", Age = 25 }
+};
+
+// Utilizando expressão lambda para filtrar pessoas com idade superior a 25 anos
+IEnumerable<Person> filteredPeople = people.Where(p => p.Age > 25);
+
+// Imprimindo os nomes das pessoas filtradas
+foreach (var person in filteredPeople)
+{
+    Console.WriteLine(person.Name);
+}
