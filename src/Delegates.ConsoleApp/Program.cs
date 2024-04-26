f06f9af396d930916d4cb31353dfe1026caee860
kauatwn <kaualopescontato@gmail.com>
2024-04-25 23:59:26 -0300

    Delegates

---
+++ src/Delegates.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Delegates.ConsoleApp.Services;
+
+double value1 = 20.0;
+double value2 = 20.0;
+
+ArithmeticOperation operation = CalculationService.Sum;
+
+double result =  operation(value1, value2);
+Console.WriteLine(result);
+
+delegate double ArithmeticOperation(double number1, double number2);
---
+++ src/Delegates.ConsoleApp/Services/CalculationService.cs
@@ -0,0 +1,15 @@
+﻿namespace Delegates.ConsoleApp.Services
+{
+    internal class CalculationService
+    {
+        public static double Sum(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public static double GetMax(double number1, double number2)
+        {
+            return number1 > number2 ? number1 : number2;
+        }
+    }
+}
