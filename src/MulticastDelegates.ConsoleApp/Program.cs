25ed4190ea3a208261385e291f98e519e1b96512
kauatwn <kaualopescontato@gmail.com>
2024-04-27 14:33:09 -0300

    Multicast delegates

---
+++ src/MulticastDelegates.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using MulticastDelegates.ConsoleApp.Services;
+
+static void PerformOperations(BinaryOperation operation, double number1, double number2)
+{
+    Console.WriteLine("Performing operations:");
+    operation.Invoke(number1, number2);
+}
+
+double number1 = 20.0;
+double number2 = 10.0;
+
+// Combining delegates (multicast delegates)
+BinaryOperation operation = CalculationService.Sum;
+operation += CalculationService.Subtract;
+operation += CalculationService.Divide;
+operation += CalculationService.GetLargerNumber;
+
+
+// Delegate as variable
+operation.Invoke(number1, number2);
+Console.WriteLine();
+
+
+// Delegate as argument
+PerformOperations(operation, number1, number2);
---
+++ src/MulticastDelegates.ConsoleApp/Services/CalculationService.cs
@@ -0,0 +1,31 @@
+﻿namespace MulticastDelegates.ConsoleApp.Services
+{
+    internal delegate void BinaryOperation(double number1, double number2);
+
+    internal class CalculationService
+    {
+        public static void Sum(double number1, double number2)
+        {
+            double result = number1 + number2;
+            Console.WriteLine($"{number1} plus {number2} equals {result}.");
+        }
+
+        public static void Subtract(double number1, double number2)
+        {
+            double result = number1 - number2;
+            Console.WriteLine($"{number1} minus {number2} equals {result}.");
+        }
+
+        public static void Divide(double number1, double number2)
+        {
+            double result = number1 / number2;
+            Console.WriteLine($"{number1} divided by {number2} equals {result}.");
+        }
+
+        public static void GetLargerNumber(double number1, double number2)
+        {
+            double result = number1 > number2 ? number1 : number2;
+            Console.WriteLine($"The largerst number between {number1} and {number2} is {result}.");
+        }
+    }
+}
