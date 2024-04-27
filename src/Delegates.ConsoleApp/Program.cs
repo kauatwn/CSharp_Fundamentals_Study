e83eb476ba5efa7648297899f083e053254f843c
kauatwn <kaualopescontato@gmail.com>
2024-04-27 13:03:25 -0300

    Delegates

--- src/Delegates.ConsoleApp/Program.cs
+++ src/Delegates.ConsoleApp/Program.cs
@@ -1,11 +1,24 @@
 ﻿using Delegates.ConsoleApp.Services;
 
-double value1 = 20.0;
-double value2 = 20.0;
+static double PerformOperation(BinaryOperation binaryOperation, double number1, double number2)
+{
+    return binaryOperation(number1, number2);
+}
 
-ArithmeticOperation operation = CalculationService.Sum;
+double number1 = 20.0;
+double number2 = 10.0;
 
-double result =  operation(value1, value2);
-Console.WriteLine(result);
+// Delegates as arguments
+double sumResult = PerformOperation(CalculationService.Sum, number1, number2);
+Console.WriteLine($"{number1} plus {number2} equals {sumResult}.");
 
-delegate double ArithmeticOperation(double number1, double number2);
+
+// Delegate as variable
+BinaryOperation operation = CalculationService.Subtraction;
+double subtractionResult = operation(number1, number2);
+Console.WriteLine($"{number1} minus {number2} equals {subtractionResult}.");
+
+
+// Lambda expression as a delegate
+double largerstNumber = PerformOperation((n1, n2) => CalculationService.GetLarger(n1, n2), number1, number2);
+Console.WriteLine($"The largerst number between {number1} and {number2} is {largerstNumber}.");
--- src/Delegates.ConsoleApp/Services/CalculationService.cs
+++ src/Delegates.ConsoleApp/Services/CalculationService.cs
@@ -1,5 +1,7 @@
 ﻿namespace Delegates.ConsoleApp.Services
 {
+    internal delegate double BinaryOperation(double number1, double number2);
+
     internal class CalculationService
     {
         public static double Sum(double number1, double number2)
@@ -7,7 +9,12 @@
             return number1 + number2;
         }
 
-        public static double GetMax(double number1, double number2)
+        public static double Subtraction(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public static double GetLarger(double number1, double number2)
         {
             return number1 > number2 ? number1 : number2;
         }
