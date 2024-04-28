402df76801da1e67c93918e85ff1d46d05ba53a2
kauatwn <kaualopescontato@gmail.com>
2024-04-28 14:29:40 -0300

    Func delegate

---
+++ src/FuncDelegate.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using FuncDelegate.ConsoleApp.Entities;
+
+var products = new List<Product>
+{
+    new("TV", 1250.0),
+    new("Mouse", 100.0),
+    new("Tablet", 250.0),
+    new("HD Case", 50.0),
+};
+
+Func<Product, string> productNameToUpperFunc = ProductNameToUpper;
+
+List<string> productNamesInUpperCase = products.Select(productNameToUpperFunc).ToList();
+
+foreach (var product in products)
+{
+    Console.WriteLine(product);
+}
+
+static string ProductNameToUpper(Product p)
+{
+    return p.Name.ToUpper();
+}
+
+// Func<T> delegate encapsula métodos que retornam valores TResult e pode receber até 16 parâmetros de entrada
