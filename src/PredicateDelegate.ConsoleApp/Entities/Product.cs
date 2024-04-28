c4cad4b5973eb74cbd967fb235e20a4c84e55f2c
kauatwn <kaualopescontato@gmail.com>
2024-04-28 11:52:14 -0300

    Predicate delegate

---
+++ src/PredicateDelegate.ConsoleApp/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace PredicateDelegate.ConsoleApp.Entities
+{
+    internal class Product(string name, double price)
+    {
+        public string Name { get; set; } = name;
+        public double Price { get; set; } = price;
+
+        public override string ToString()
+        {
+            return $"{Name}, {Price.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ src/PredicateDelegate.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using PredicateDelegate.ConsoleApp.Entities;
+
+var products = new List<Product>
+{
+    new("TV", 1250.0),
+    new("Mouse", 100.0),
+    new("Tablet", 250.0),
+    new("HD Case", 50.0),
+};
+
+Predicate<Product> expensiveProductPredicate = IsExpensive;
+
+products.RemoveAll(expensiveProductPredicate);
+
+foreach (var product in products)
+{
+    Console.WriteLine(product);
+}
+
+static bool IsExpensive(Product product)
+{
+    return product.Price >= 1000.0;
+}
+
+// Predicate<T> delegate representa um método que recebe um objeto T e retorna um valor booleano
