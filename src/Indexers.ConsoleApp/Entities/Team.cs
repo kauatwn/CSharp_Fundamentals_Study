e8d5ff9124f0a3d912b36a2b954dec1dba6f3a0a
kauatwn <kaualopescontato@gmail.com>
2024-05-05 14:44:50 -0300

    Indexers

---
+++ src/Indexers.ConsoleApp/Entities/Team.cs
@@ -0,0 +1,34 @@
+﻿namespace Indexers.ConsoleApp.Entities
+{
+    internal class Team
+    {
+        private string[] Players { get; } = new string[11];
+
+        public string this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < Players.Length)
+                {
+                    return Players[i];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Índice de jogador fora dos limites da equipe.");
+                }
+            }
+
+            set
+            {
+                if (i >= 0 && i < Players.Length)
+                {
+                    Players[i] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Índice de jogador fora dos limites da equipe.");
+                }
+            }
+        }
+    }
+}
---
+++ src/Indexers.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Indexers.ConsoleApp.Entities;
+
+var team = new Team();
+
+// Using indexer
+team[0] = "Lionel Messi";
+team[1] = "Cristiano Ronaldo";
+team[2] = "Neymar Júnior";
+
+Console.WriteLine("O melhor jogador é: " + team[0]);
+Console.WriteLine("O segundo melhor jogador é: " + team[1]);
+Console.WriteLine("O terceiro melhor jogador é: " + team[2]);
+
+// Not using indexer
+//team[0].Players = "Lionel Messi";
+//team[1].Players = "Cristiano Ronaldo";
+//team[2].Players = "Neymar Júnior";
+
+//Console.WriteLine("O melhor jogador é: " + team.Players[0]);
+//Console.WriteLine("O segundo melhor jogador é: " + team.Players[1]);
+//Console.WriteLine("O terceiro melhor jogador é: " + team.Players[2]);
