ef024abb4a950dca662167544bd4034ed6ff1e4a
kauatwn <kaualopescontato@gmail.com>
2024-05-30 00:11:33 -0300

    Switch expression

---
+++ src/SwitchExpression.ConsoleApp/DrinkType.cs
@@ -0,0 +1,10 @@
+﻿namespace SwitchExpression.ConsoleApp
+{
+    internal enum DrinkType
+    {
+        Coffee = 1,
+        Tea = 2,
+        Juice = 3,
+        Soda = 4
+    }
+}
---
+++ src/SwitchExpression.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using SwitchExpression.ConsoleApp;
+
+Console.WriteLine("Choose a drink type:\n");
+Console.WriteLine("1. Coffee");
+Console.WriteLine("2. Tea");
+Console.WriteLine("3. Juice");
+Console.WriteLine("4. Soda\n");
+
+int option = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+DrinkType chosenDrink = (DrinkType)option;
+
+decimal price = chosenDrink switch
+{
+    DrinkType.Coffee => 3.50m,
+    DrinkType.Tea => 2.00m,
+    DrinkType.Juice => 5.00m,
+    DrinkType.Soda => 4.00m,
+    _ => throw new ArgumentException("Invalid option.")
+};
+
+Console.WriteLine($"The price of the chosen drink is: $ {price}");
