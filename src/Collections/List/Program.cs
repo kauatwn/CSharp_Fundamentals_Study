9426f44ac0ad383fcaccaf7a0fd4be5448787fb5
kauatwn <kaualopescontato@gmail.com>
2024-12-15 13:08:51 -0300

    List

--- src/Collections/List/Program.cs
+++ src/Collections/List/Program.cs
@@ -1,15 +1,11 @@
-﻿var list = new List<string> { "Maria", "Alex" };
-
-/* Outra forma de criar lista
- * 
- * List<string> list = ["Maria", "Alex"];
- */
+﻿List<string> list = ["Maria", "Alex"];
 
 // Imprime todos os itens da lista
-foreach (var item in list)
+foreach (string item in list)
 {
     Console.WriteLine(item);
 }
+
 Console.WriteLine("----------");
 
 // Insere na segunda posição da lista
@@ -17,15 +13,16 @@ list.Insert(2, "Marco");
 
 // Adiciona ao final da lista
 list.Add("Anna");
-foreach (var item in list)
+foreach (string item in list)
 {
     Console.WriteLine(item);
 }
+
 Console.WriteLine("----------");
 
 // Obtém nomes com tamanho 5
-var list2 = list.FindAll(item => item.Length == 5);
-foreach (var name in list2)
+List<string> list2 = list.FindAll(item => item.Length == 5);
+foreach (string name in list2)
 {
     Console.WriteLine(name);
 }
